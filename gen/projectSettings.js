3a33830415e890efbb810345de97c45cd5447f4a
iver56 <iver56@hotmail.com>
2018-09-30 14:18:21 +0200

    Init nin project. Add music draft.

---
+++ gen/projectSettings.js
@@ -0,0 +1,1 @@
+var PROJECT = {"title":"beesandbombs tribute","authors":["iverjo","haakon"],"description":"","previewImage":"https://raw.githubusercontent.com/ninjadev/nin/master/nin/frontend/app/images/nin-dark.png","music":{"path":"res/music.mp3","bpm":79,"subdivision":4,"BEANOffset":0},"googleAnalyticsID":"Your GA ID","labels":[{"name":"Start","BEAN":0}]};
