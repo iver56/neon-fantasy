b153a6c4b5d35125f2bed70328b6ad4cd4431b89
iver56 <iver56@hotmail.com>
2018-10-13 01:24:42 +0200

    Use same geometry for all stars in castle scene. Without this patch, the mobile device would run out of memory and crash.

--- src/neonCityNode.js
+++ src/neonCityNode.js
@@ -72,8 +72,8 @@
 
       // STARS
       this.spheres = [];
+      var starGeometry = new THREE.SphereGeometry(10, 10, 10);
 
-      
       for (let i = 0; i < 600; i++) 
       {
         var color = new THREE.Color();
@@ -82,10 +82,8 @@
           .5,
           .5
           );
-        
         var starMaterial = new THREE.MeshBasicMaterial({emissiveColor: color.getHex()});
-        var sphere = new THREE.Mesh(new THREE.SphereGeometry(10, 32, 32),
-          starMaterial);
+        var sphere = new THREE.Mesh(starGeometry, starMaterial);
         
         sphere.position.y =  Math.random() * 4000;
         sphere.position.x = -4000 + Math.random() * 8000;
